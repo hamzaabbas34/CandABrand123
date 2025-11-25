abb92012f08e20b99e6bc6aafc4135c7aa6b2efc
Hamza abbas <129648939+hamzaabbas34@users.noreply.github.com>
2025-11-25 23:16:22 +0500

    herosectionmodel

---
+++ models/HeroSection.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const heroSectionSchema = new mongoose.Schema(
+	{
+		brand: {
+			type: String,
+			required: true,
+			enum: ["Azure", "Monsini", "Risky"],
+		},
+		category: {
+			type: String,
+			required: true,
+		},
+		media: [
+			{
+				url: {
+					type: String,
+					required: true,
+				},
+				type: {
+					type: String,
+					required: true,
+					enum: ["image", "video"],
+				},
+				filename: {
+					type: String,
+					required: true,
+				},
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+);
+
+module.exports = mongoose.model("HeroSecti", heroSectionSchema);

feb21c4818062572bdee91db14c73280ab073db6
Hamza abbas <129648939+hamzaabbas34@users.noreply.github.com>
2025-12-05 22:14:33 +0500

    dfxcgvbhnj

--- routes/contact.js
+++ routes/contact.js
@@ -1,6 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { contactMe , newSubscriber } = require('../controllers/contactController.js')
-router.post('/contact' ,  contactMe )
-router.post('/subscriber' ,  newSubscriber )
+const {
+	contactMe,
+	newSubscriber,
+} = require("../controllers/contactController.js");
+const { navbar } = require("../controllers/productController.js");
+router.post("/contact", contactMe);
+router.post("/subscriber", newSubscriber);
+router.post("/navbar", navbar);
 module.exports = router;
